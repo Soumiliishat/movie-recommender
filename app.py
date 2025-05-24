092906d046bbbf108ad790bbd3907ba74f51f744
Soumiliishat <155948513+Soumiliishat@users.noreply.github.com>
2025-05-25 03:03:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def recommend(movie):
 # Load saved data
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+similarity = pickle.load(open('data/similarity.pkl', 'rb'))
 
 
 # Fetch trending movies from TMDb
